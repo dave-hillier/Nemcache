7181f44747a8ed2e7c78234a51bcc3a8bbecf0dd
Dave Hillier <dave.hillier1@gmail.com>
2013-04-26 22:39:55 +0100

    Fixed missed files

---
+++ Nemcache.Service/Nemcache.Service/Journaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Nemcache.Service.Notifications;
+
+namespace Nemcache.Service
+{
+    class Journaller
+    {
+        private Stream _outputStream;
+
+        public Journaller(Stream outputStream, IObservable<ICacheNotification> notifications)
+        {
+            _outputStream = outputStream;
+        }
+        
+    }
+}
